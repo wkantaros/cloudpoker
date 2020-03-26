2716f398bff3fedeb3b64b8d6b47930207fcbde8
Will Kantaros <wkantaros@gmail.com>
2020-03-26 02:47:48 -0400

    significant work with sockets

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // Dependencies
+let poker = require('./poker-logic/lib/node-poker');
 let express = require('express');
 let http = require('http');
 let path = require('path');
@@ -10,10 +11,19 @@ let server = http.Server(app);
 // app.use('/static', express.static(__dirname + '/static'));
 // app.use(express.static('public'));
 app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/login'));
 
 // Routing
 app.get('/', function (request, response) {
-    response.sendFile(path.join(__dirname, 'index.html'));
+    response.sendFile(path.join(__dirname, '/login/index.html'));
+});
+
+app.get('/login', function (request, response) {
+    response.sendFile(path.join(__dirname, '/public/login.html'));
+});
+
+app.get('/game', function (request, response) {
+    response.sendFile(path.join(__dirname, 'public/game.html'));
 });
 
 // Starts the server.
@@ -21,32 +31,174 @@ server.listen(5000, function () {
     console.log('Starting server on port 5000');
 });
 
-// Starts the server.
-// let server = app.listen(5000, function () {
-//     console.log('Starting server on port 5000');
-// });
-
 //socket setup
 let io = socketIO(server);
 // Add the WebSocket handlers
-
+var table = new poker.Table(25, 50, 2, 10, 50, 50000);
 io.on('connection', (socket) => { // this is setting up the socket on the server
     console.log('made socket connection', socket.id);
 
     socket.on('chat', (data) => {
-        console.log(data);
+        // console.log(data);
         //recieving message from socket and emmitting it back to all sockets
         io.sockets.emit('chat', data);
     });
 
     socket.on('typing', (handle) => {
-        console.log(handle);
+        // console.log(handle);
         // bradcast = everyone gets it but the sender
         socket.broadcast.emit('typing', handle);
     });
 
+    socket.on('buy-in', (data) => {
+        console.log(`${data.name} buys in for ${data.stack}`);
+        table.AddPlayer(data.name, data.stack);
+        player_ids[data.name] = data.id;
+        io.sockets.emit('buy-in', data);
+    });
+
+    socket.on('buy-out', (data) => {
+        console.log(`${data.name} buys out for ${data.stack}`);
+        table.removePlayer(data.name);
+        io.sockets.emit('buy-out', data);
+    });
+
+    socket.on('start-game', (data) => {
+        table.StartGame();
+        begin_round();
+    });
+
+    socket.on('check', (data) => {
+        if (table.getCurrentPlayer() === data.username){
+            prev_round = table.game.roundName;
+            let able_to_check = table.check(table.getCurrentPlayer());
+            if (able_to_check){
+                io.sockets.emit('check', {
+                    username: data.username,
+                    stack: table.getPlayer(data.username).chips,
+                    pot: table.game.pot
+                });
+                // update client's stack size
+                io.to(`${player_ids[data.username]}`).emit('update-stack', {
+                    stack: table.getPlayer(data.username).chips
+                });
+                io.to(`${player_ids[table.getCurrentPlayer()]}`).emit('action', {
+                    canCheck: able_to_check
+                });
+            }
+            check_round(prev_round);
+        }
+    });
+
+    socket.on('call', (data) => {
+        if (table.getCurrentPlayer() === data.username) {
+            prev_round = table.game.roundName;
+            table.call(table.getCurrentPlayer());
+            // send call back to every client
+            io.sockets.emit('call', {
+                username: data.username,
+                stack: table.getPlayer(data.username).chips,
+                pot: table.game.pot
+            });
+            // update client's stack size
+            io.to(`${player_ids[data.username]}`).emit('update-stack', {
+                stack: table.getPlayer(data.username).chips
+            });
+            // let next client know it's his action
+            io.to(`${player_ids[table.getCurrentPlayer()]}`).emit('action', {});
+            // console.log(`Current player ${table.currentPlayer} (${data.username}) calls`);
+            check_round(prev_round);
+        }
+    });
+
+    socket.on('bet', (data) => {
+        if (table.getCurrentPlayer() === data.username) {
+            prev_round = table.game.roundName;
+            table.bet(table.getCurrentPlayer(), data.amount);
+            io.sockets.emit('bet', {
+                username: data.username,
+                stack: table.getPlayer(data.username).chips,
+                amount: data.amount,
+                pot: table.game.pot
+            }); 
+            // update client's stack size
+            io.to(`${player_ids[data.username]}`).emit('update-stack', {
+                stack: table.getPlayer(data.username).chips
+            });
+            io.to(`${player_ids[table.getCurrentPlayer()]}`).emit('action', {});
+            // console.log(`${data.username} bets ${data.amount}`);
+            check_round(prev_round);
+        }
+    });
+    socket.on('fold', (data) => {
+        if (table.getCurrentPlayer() === data.username) {
+            prev_round = table.game.roundName;
+            table.fold(table.getCurrentPlayer());
+            io.sockets.emit('fold', {
+                username: data.username,
+                stack: table.getPlayer(data.username).chips,
+                pot: table.game.pot
+            });
+            // update client's stack size
+            io.to(`${player_ids[data.username]}`).emit('update-stack', {
+                stack: table.getPlayer(data.username).chips
+            });
+            io.to(`${player_ids[table.getCurrentPlayer()]}`).emit('action', {});
+            // console.log(`Current player ${table.currentPlayer} (${data.username}) folds`);
+            check_round(prev_round);
+        }
+    });
 });
 
-// setInterval(function () {
-//     io.sockets.emit('message', 'hi!');
-// }, 1000);
+//checks if round has ended (reveals next card)
+let check_round = (prev_round) => {
+    console.log(table);
+    let data = table.checkwin();
+    if (table.game.roundName === 'Showdown'){
+        winners = table.getWinners();
+        io.sockets.emit('showdown', winners);
+        table.initNewRound();
+        begin_round();
+    } else if (data.everyoneFolded) {
+        console.log(`${data.winner.playerName} won a pot of ${data.pot}`);
+        io.to(`${player_ids[data.winner.playerName]}`).emit('update-stack', {
+            stack: data.winner.chips + data.pot
+        });
+        data.winner.GetChips(data.pot);
+        console.log(data.winner.chips);
+        io.sockets.emit('folds-through', {
+            username: data.winner.playerName
+        });
+        table.initNewRound();
+        begin_round();
+    }
+    else if (prev_round !== table.game.roundName){
+        io.sockets.emit('display-board', {
+            street: table.game.roundName,
+            board: table.getDeal()
+        });
+    }
+}
+
+var player_ids = {}
+
+// let update_client_stacks = () => {
+
+// }
+
+let begin_round = () => {
+    for (let i = 0; i < table.players.length; i++) {
+        let name = table.players[i].playerName;
+        let chips = table.players[i].chips;
+        io.to(`${player_ids[name]}`).emit('start-hand', {
+            username: name,
+            cards: table.getHandForPlayerName(name),
+            stack: chips
+        });
+        io.to(`${player_ids[name]}`).emit('update-stack', {
+            stack: table.getPlayer(name).chips
+        });
+
+    }
+    io.to(`${player_ids[table.getCurrentPlayer()]}`).emit('action', {});
+}
--- poker-logic/example.js
+++ poker-logic/example.js
@@ -1,35 +1,95 @@
 var poker = require('./lib/node-poker');
 
-var table = new poker.Table(50,100,4,10,100,1000);
+var table = new poker.Table(50,100,2,10,100,1000);
 
-table.AddPlayer('bob',1000)
-table.AddPlayer('jane',1000)
-table.AddPlayer('dylan',1000)
-table.AddPlayer('john',1000)
+table.AddPlayer('bob',1000) //dealer
+table.AddPlayer('jane',1000) //small blind
+table.AddPlayer('dylan',1000) //big blind
+table.AddPlayer('john',400) //first to act preflop
+table.AddPlayer('eve',1000) //second to act preflop
 
+let whosAction = () => console.log(`We are on the ${table.game.roundName}, it is ${table.getCurrentPlayer()}\'s action`);
 
 table.StartGame()
-console.log(table);
-table.players[1].Call();
-table.players[2].Call();
-table.players[3].Call();
-table.players[0].Call();
-table.players[1].Call();
-table.players[2].Call();
-table.players[3].Call();
-table.players[0].Call();
-table.players[1].Bet(50);
-table.players[2].Bet(1);
-console.log( table.players[1] );
-console.log( table.players[2] );
-table.players[3].Call();
-table.players[0].Call();
-table.players[1].Call();
-table.players[2].Call();
-table.players[3].Call();
-table.players[0].Call();
+// console.log(table);
 
-console.log(table.game);
 
-//table.initNewRound()
+//preflop
+console.log(table.game.pot);
+table.call('john');
+console.log(table.game.pot);
+whosAction();
+table.call('eve');
+whosAction();
+table.call('bob');
+whosAction();
+table.call('jane');
+whosAction();
+table.call('dylan');
+// console.log(table);
+// console.log(table.checkwin().winner.table.game.roundBets);
+let obj = table.checkwin();
+if (obj.everyoneFolded){
+    console.log(`${obj.winner.playerName} won a pot of ${obj.pot}`);
+    obj.winner.GetChips(obj.pot);
+}
+
+// table.initNewRound();
+// whosAction();
+// table.fold('eve');
+// whosAction();
+// console.log(table);
+
+// table.checkForWinner1();
+
+//flop
+console.log(table.getDeal());
+whosAction();
+table.check('jane');
+whosAction();
+table.check('dylan');
+whosAction();
+table.check('john');
+whosAction();
+table.check('eve');
+whosAction();
+table.check('bob');
+
+//turn
+console.log(table.getDeal());
+whosAction();
+table.bet('jane', 50);
+whosAction();
+table.bet('dylan', 100);
+whosAction();
+table.call('john');
+whosAction();
+table.fold('eve');
+table.AddPlayer('kanty', 1000);
+whosAction();
+table.call('bob');
+whosAction();
+table.call('jane');
+
+//river
+console.log(table.getDeal());
+whosAction();
+table.check('jane');
+whosAction();
+table.bet('dylan', 100);
+whosAction();
+table.call('john');
+whosAction();
+table.call('bob');
+whosAction();
+table.fold('jane');
+whosAction();
+console.log(table.getWinners());
+
+// // table.initNewRound();
+// // console.log(`We are on the ${table.game.roundName}, it is ${table.getCurrentPlayer()}\'s action`);
+
+// // // console.log(table);
+// // // console.log(`We are on the ${table.game.roundName}, it is ${table.getCurrentPlayer()}\'s action`);
+
 
