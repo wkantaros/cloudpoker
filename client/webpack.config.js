f85c1b77a57b2bec83df3709ef80879bdfbb5366
wardbradt <wardbradt5@gmail.com>
2020-05-04 10:37:06 -0400

    start using wp

---
+++ client/webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(m?js$|jsx)/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react']
+                    }
+                }
+            },
+            {
+                 test: /\.css$/,
+                 use: [
+                   'style-loader',
+                   'css-loader',
+                 ],
+           },
+        ]
+    }
+};
