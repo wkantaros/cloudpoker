d0346bd66e869e164fce3d56591bb984cd5f00e1
wardbradt <wardbradt5@gmail.com>
2020-05-19 17:20:36 -0400

    close logs when on "esc" keydown

--- client/src/components/gameLog.jsx
+++ client/src/components/gameLog.jsx
@@ -1,6 +1,24 @@
 import React, {Component} from "react";
 
 export default class GameLog extends Component {
+    constructor(props) {
+        super(props);
+        this.handleKeyDown = this.handleKeyDown.bind(this);
+    }
+    handleKeyDown(e) {
+        // esc key
+        if (e.keyCode === 27) {
+            e.stopPropagation();
+            this.props.onClose(e);
+        }
+    }
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        if (this.props.width === "100%" && this.props.width !== prevProps.width) {
+            document.addEventListener('keydown', this.handleKeyDown);
+        } else if (this.props.width !== "100%" && this.props.width !== prevProps.width) {
+            document.removeEventListener('keydown', this.handleKeyDown);
+        }
+    }
     render() {
         return (
             <div id="game-log" className="overlay" style={{width: this.props.width}}>
