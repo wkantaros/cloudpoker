66cbac90335d425ac8ebf56177ee88b7f1b12c0c
wardbradt <wardbradt5@gmail.com>
2020-05-19 17:13:49 -0400

    show game log

--- client/src/components/gameLog.jsx
+++ client/src/components/gameLog.jsx
@@ -3,7 +3,7 @@ import React, {Component} from "react";
 export default class GameLog extends Component {
     render() {
         return (
-            <div id="game-log" className="overlay">
+            <div id="game-log" className="overlay" style={{width: this.props.width}}>
                 <a onClick={this.props.onClose} className="closebtn" id="closeLog">&times;</a>
                 <div className="overlay-content">
                     <a href="#">Game Log</a>
